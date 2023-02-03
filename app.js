b2095870b62491a07ee6914c221eaafe9205ee6b
js-ssfsdcs <20366060+js-ssfsdcs@users.noreply.github.com>
2023-02-02 19:13:32 -0800

    server to accept post requests

---
+++ app.js
@@ -0,0 +1,33 @@
+const port = process.env.PORT || 5000
+const express = require('express')
+const app = express()
+const bodyParser = require('body-parser');
+ 
+app.use(bodyParser.urlencoded({ extended: true }));
+
+app.get('/checks', function (req, res) {
+    res.send('checks')
+})
+
+app.post('/accepts', function (req, res) {
+  
+    res.header('Access-Control-Allow-Origin', '*')
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
+    
+    var logTxt = "FORM SUBMISSION: "
+    var txt = '<style>table, th, td { border: 1px solid black; border-collapse: collapse; } th, td { padding: 10px; </style><h3>Form submission</h3> <table>'
+    
+    
+    for(n in req.body) {
+        logTxt += n + '=' + req.body[n] + ','
+        txt += '<tr><td><strong>' + n + '</strong></td><td>' + req.body[n] + '</td></tr>'
+    }
+
+    txt += '</table>'
+    console.log(logTxt)
+    res.send(txt)
+    
+})
+
+app.listen(port, () => console.log(`Example app listening at http://localhost:${port}`))
