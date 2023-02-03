10bee0917bc27bb2f6cae847a8cb22e452641d04
js-ssfsdcs <20366060+js-ssfsdcs@users.noreply.github.com>
2023-02-02 20:11:01 -0800

    mlem, sir

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ const express = require('express')
 const app = express()
 const bodyParser = require('body-parser');
  
+app.use(express.static('public'))
 app.use(bodyParser.urlencoded({ extended: true }));
 
 app.get('/checks', function (req, res) {
