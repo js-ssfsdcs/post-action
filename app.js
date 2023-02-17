cda17543c522bce85af5f9e0db8b64de59618d8f
js-ssfsdcs <20366060+js-ssfsdcs@users.noreply.github.com>
2023-02-17 04:46:47 -0800

    better readability with template literals

--- app.js
+++ app.js
@@ -1,32 +1,48 @@
 const port = process.env.PORT || 5000
 const express = require('express')
 const app = express()
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser')
+
+function allowCORS(response) {
+    response.header('Access-Control-Allow-Origin', '*')
+    response.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
+    response.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
+}
 
 app.use(bodyParser.urlencoded({ extended: true }));
 
-app.get(['/', '/checks'], function (req, res) {
-    res.send('POST your form action here!')
+app.get(['/', '/checks'], function (request, response) {
+    response.send('POST your form action here!')
 })
 
-app.post(['/','/accepts'], function (req, res) {
-  
-    res.header('Access-Control-Allow-Origin', '*')
-    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS')
-    res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization, Content-Length, X-Requested-With')
-    
-    var logTxt = 'FORM SUBMISSION: '
-    var txt = '<style>table, th, td { border: 1px solid black; border-collapse: collapse; } th, td { padding: 10px; </style><h3>Form submission</h3> <table>'
-    
-    
-    for(n in req.body) {
-        logTxt += n + '=' + req.body[n] + ','
-        txt += '<tr><td><strong>' + n + '</strong></td><td>' + req.body[n] + '</td></tr>'
-    }
+app.post(['/','/accepts'], function (request, response) {
 
-    txt += '</table>'
-    console.log(logTxt)
-    res.send(txt)
+    let logText = 'FORM SUBMISSION: '
+    let html = `<style>
+                    table, th, td { 
+                        border: 1px solid black; 
+                        border-collapse: collapse; 
+                    } 
+                    
+                    th, td { 
+                        padding: 10px; 
+                    }
+                </style>
+                <h3>Form submission</h3> 
+                <table>`
+    
+    for(key in request.body) {
+        logText += `${key}=${request.body[key]},`
+        html += `<tr>
+                    <td><strong>${key}</strong></td>
+                    <td>${request.body[key]}</td>
+                 </tr>`
+    }
+    html += '</table>'
+
+    console.log(logText)
+    allowCORS(response)
+    response.send(html)
 })
 
 app.use(express.static('public'))
